ed46cdcf5f7bd4caaa30ba34543903d998b40d69
macarena <macarena.c.rios@gmail.com>
2025-04-11 12:00:13 +0200

    modoSimple

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// 1️⃣Crea un input donde el usuario pueda escribir su nombre y un botón con el texto "Say hello".
+// Al pulsar el botón, se debe crear e insertar un elemento h2 con el texto "Hello, [nombre]".
+
+// 💡 Detalles técnicos que deben aplicar:
+// El h2 debe generarse dinámicamente con createElement.
+// El texto debe construirse a partir del valor del input, si está vacío o tiene menos de 3 letras no se insertará nada.
+
+const textElement = document.getElementById('text');
+const buttonElement = document.getElementById('button');
+const newName = document.createElement('h3');
+const newDiv = document.createElement('h3');
+
+const sayHello = event => {
+  const textContentInput = textElement.value;
+
+  newName.textContent = `Hello ${textContentInput}`;
+  newDiv.prepend(newName);
+
+  document.body.append(newDiv);
+};
+
+buttonElement.addEventListener('click', sayHello);
+textElement.addEventListener('submit', sayHello);
